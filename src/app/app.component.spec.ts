b6f89fb858f0a61a49b564849eb5b3392d9942b1
anitaSzabo <azorka3@gmail.com>
2019-01-16 13:40:40 +0100

    init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,13 +19,13 @@ describe('AppComponent', () => {
   it(`should have as title 'recipe'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('recipe');
+    expect(app.title).toEqual('recipe-detail');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipe!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to recipe-detail!');
   });
 });
